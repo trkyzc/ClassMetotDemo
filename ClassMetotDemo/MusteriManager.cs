8b32380d2c3efaf1e636b3c8ac8b710b8671b8f6
Tarık Yazıcı <tarik_yazici_1905@hotmail.com>
2021-02-14 02:22:33 +0300

    Add project files.

---
+++ ClassMetotDemo/MusteriManager.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ClassMetotDemo
+{
+    class MusteriManager
+    {
+        public void MusteriEkleme(Musteri musteri)
+        {
+            Console.WriteLine(musteri.Ad + musteri.Soyad + " basarıyla eklendi.");
+        }
+        public void MusteriSilme(Musteri musteri)
+        {
+            Console.WriteLine(musteri.Ad + musteri.Soyad + " basarıyla silindi.");
+        }
+        public void MusteriListeleme()
+        {
+            
+        }
+    }
+}
---
+++ ClassMetotDemo/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+
+namespace ClassMetotDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Musteri musteri1 = new Musteri();
+            musteri1.Ad = "Mehmet ";
+            musteri1.Soyad = "Yıldırım";
+            musteri1.TelNo =5365671234;
+            musteri1.MusteriNo = 01;
+
+            Musteri musteri2 = new Musteri();
+            musteri2.Ad = "Mert ";
+            musteri2.Soyad = "Günes";
+            musteri2.TelNo =5361234567;
+            musteri2.MusteriNo = 02;
+
+            
+
+            MusteriManager musteriManager = new MusteriManager();
+            musteriManager.MusteriEkleme(musteri1);
+            
+            musteriManager.MusteriEkleme(musteri2);
+            
+            musteriManager.MusteriSilme(musteri1);
+            
+            musteriManager.MusteriSilme(musteri2);
+            
+
+
+            musteriManager.MusteriListeleme();
+            Console.WriteLine("Musteriler listeleniyor...");
+            Console.WriteLine("*************");
+
+            Musteri[] musteriler = new Musteri[] { musteri1, musteri2 };
+            foreach (Musteri musteri in musteriler)
+            {
+                Console.WriteLine(musteri.Ad);
+                Console.WriteLine(musteri.Soyad);
+                Console.WriteLine(musteri.TelNo);
+                Console.WriteLine(musteri.MusteriNo);
+                Console.WriteLine("*****************");
+
+            }
+
+
+        }
+    }
+}
